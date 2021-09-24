740827e7518e995addb9dc7bb6d8523ee183cd80
Harish-GetMega <harish.kumar@getmega.com>
2021-09-24 09:18:05 +0530

    game init

---
+++ SwiftUILearning/CardsMemoryGame/Shared/CardsMemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsMemoryGameApp.swift
+//  Shared
+//
+//  Created by Harish on 24/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CardsMemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
